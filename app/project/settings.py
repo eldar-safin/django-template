796fd826fb90ff0af031d166f6a0b82902811e05
Eldar Safin <github@eldar-s.online>
2023-07-11 13:20:03 +0300

    Fix static folder setting

--- app/project/settings.py
+++ app/project/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR.joinpath('static')
+STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
