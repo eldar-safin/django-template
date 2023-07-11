3589081752e99212ceb7279735223511e9a34ada
Eldar Safin <github@eldar-s.online>
2023-07-11 11:44:57 +0300

    Add static folders settings

--- app/project/settings.py
+++ app/project/settings.py
@@ -103,9 +103,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR.joinpath('static')
-]
+STATIC_ROOT = BASE_DIR.joinpath('static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
